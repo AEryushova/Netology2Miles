0757b80fba4ce1f04846a2fac259cbb686c347fc
Anna Eryushova <eryushovaa@yandex.ru>
2022-12-03 15:22:58 +0400

    Miles

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 27599;
+        int rub = 20;
+        int miles = (ticket / rub);
+        System.out.println(miles);
+
+    }
+}
